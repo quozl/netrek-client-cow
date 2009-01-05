801ded41db2f8ffe6af53db520ab2bd2a4bb8da4
quozl <quozl@us.netrek.org>
2009-01-05 08:05:52 +0000

    Lars Bernhardsson, license change, relayed by Andrew Bressen
    Date: Tue, 30 Dec 2008 23:54:31 -0500
    To: James Cameron <quozl@us.netrek.org>, Karthik Arumugham <karthik@karthik.com>
    From: "Andrew K. Bressen" <bressen@mirror.to>
    Subject: A small copyright update for COW and BRMH
    
    Debian held up COW going into the "Main" section because of a
    copyright line from Lars Berhardsson (lab@mtek.chalmers.se) which gave
    permission to use but did not explicitly say that modification was
    ok. I contacted Lars and got his permission to Berkeley the code;
    please update copyright notices accordingly.
    
    --
    
    Date: Wed, 31 Dec 2008 17:26:46 +1100
    To: "Andrew K. Bressen" <bressen@mirror.to>
    Cc: Karthik Arumugham <karthik@karthik.com>
    From: James Cameron <quozl@us.netrek.org>
    Subject: Re: A small copyright update for COW and BRMH
    
    Can you provide Lars permission to me?
    
    --
    
    Date: Wed, 31 Dec 2008 11:18:51 -0500
    To: James Cameron <quozl@us.netrek.org>
    From: "Andrew K. Bressen" <bressen@mirror.to>
    Subject: Re: A small copyright update for COW and BRMH
    
    From Facebook messages (so no headers):
    
    From AKB 30-dec-2008 7:50 PM)
    
      I'm working on getting Netrek into various Linux distributions. I
      think you contributed some code way back then? Can I have your
      permission to publish your code to be under the BSD licensing
      agreement? (And hey, if you wanted to code more or come play, that'd
      be great too!)
      --andrew
    
    From Lars (30-dec-2008 7:56 PM)
    
      Oh, I did submit some code to the COW client that also ended up in
      BRMH I think.. so if the license of those clients permit it, I have no
      problem with it!
    
      As for me playing, it's been since 1994 that I last played, I can only
      imagine how bad I would be nowadays!
    
    --

--- dashboard3.c
+++ dashboard3.c
@@ -2,14 +2,31 @@
 /*
  * dashboard.c - graphic tstatw - 6/2/93
  * lab2-dashboard.c - 2nd graphic tstatw - 7/29/96
- * 
- * copyright 1993,1996 Lars Bernhardsson (lab@mtek.chalmers.se)
- * Free to use as long as this notice is left here.
  *
- * $Log: dashboard3.c,v $
- * Revision 1.1.1.1  1998/11/01 17:24:09  siegl
- * COW 3.0 initial revision
- * */
+ * Copyright (C) 1993 1996, Lars Bernhardsson (lab@mtek.chalmers.se)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "config.h"
 #include "copyright.h"
