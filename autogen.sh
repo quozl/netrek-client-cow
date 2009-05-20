0244519fc491d407d672736b27e7eacd1b7b9f37
quozl <quozl@us.netrek.org>
2009-05-20 05:58:32 +0000

    recreate config.sub on autogen

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,13 @@
 rm -f config.guess config.sub ltmain.sh
 aclocal
 libtoolize --copy
+if [ ! -f config.sub ]; then
+    # later versions of libtool silently fail to create config.sub
+    # unless --install is added, yet --install is not valid on the
+    # older versions.  later versions also support --no-warn to reduce
+    # output volume
+    libtoolize --install --copy --no-warn
+fi
 autoconf
 chmod +x debian/rules tests/build
 echo "autogen.sh completed ok"
